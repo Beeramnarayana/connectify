cbbd48126eaaa131afe12a1bea4773470997ccc7
Beeram Narayana <146806185+Beeramnarayana@users.noreply.github.com>
2025-03-22 19:23:03 +0530

    Update Index.js

--- BackEnd/Index.js
+++ BackEnd/Index.js
@@ -3,10 +3,36 @@ import dotenv from "dotenv";
 import { connectDb } from "./database/db.js";
 import cloudinary from "cloudinary";
 import cookieParser from "cookie-parser";
+import { Chat } from "./models/ChatModel.js";
 import { isAuth } from "./middlewares/isAuth.js";
 import { User } from "./models/userModel.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+import axios from 'axios';
 
-const app = express();
+const url = `https://mern-social-3e3m.onrender.com`;
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 dotenv.config();
 
 cloudinary.v2.config({
@@ -21,7 +47,29 @@ app.use(cookieParser());
 
 const port = process.env.PORT;
 
+// to get all chats of user
+app.get("/api/messages/chats", isAuth, async (req, res) => {
+  try {
+    const chats = await Chat.find({
+      users: req.user._id,
+    }).populate({
+      path: "users",
+      select: "name profilePic",
+    });
 
+    chats.forEach((e) => {
+      e.users = e.users.filter(
+        (user) => user._id.toString() !== req.user._id.toString()
+      );
+    });
+
+    res.json(chats);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 // to get all users
 app.get("/api/user/all", isAuth, async (req, res) => {
@@ -47,14 +95,23 @@ app.get("/api/user/all", isAuth, async (req, res) => {
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 
 //using routes
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/messages", messageRoutes);
 
+const __dirname = path.resolve();
 
-app.listen(port, () => {
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
   connectDb();
 });
