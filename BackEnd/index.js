5e371e06bfdaa67cc9666a9e40848d9c28e5cfed
Beeram Narayana <146806185+Beeramnarayana@users.noreply.github.com>
2025-03-25 19:13:38 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,8 +5,32 @@ import cloudinary from "cloudinary";
 import cookieParser from "cookie-parser";
 import { isAuth } from "./middlewares/isAuth.js";
 import { User } from "./models/userModel.js";
+import path from "path";
+import axios from 'axios';
 
-const app = express();
+const app=express()
+const interval = 30000;
+
+function reloadWebsite() {
+  axios
+    .get(url)
+    .then((response) => {
+      console.log(
+        `Reloaded at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 dotenv.config();
 
 cloudinary.v2.config({
@@ -14,13 +38,12 @@ cloudinary.v2.config({
   api_key: process.env.Cloudinary_Api,
   api_secret: process.env.Cloudinary_Secret,
 });
+
 //using middlewares
 app.use(express.json());
 app.use(cookieParser());
 
 const port = process.env.PORT;
-
-
 
 // to get all users
 app.get("/api/user/all", isAuth, async (req, res) => {
@@ -52,6 +75,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
