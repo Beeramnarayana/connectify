709f4d936a772faead6a32ffd724d6365e7b6308
Beeram Narayana <146806185+Beeramnarayana@users.noreply.github.com>
2025-03-25 19:16:53 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -11,26 +11,6 @@ import axios from 'axios';
 const app=express()
 const interval = 30000;
 
-function reloadWebsite() {
-  axios
-    .get(url)
-    .then((response) => {
-      console.log(
-        `Reloaded at ${new Date().toISOString()}: Status Code ${
-          response.status
-        }`
-      );
-    })
-    .catch((error) => {
-      console.error(
-        `Error reloading at ${new Date().toISOString()}:`,
-        error.message
-      );
-    });
-}
-
-setInterval(reloadWebsite, interval);
-
 dotenv.config();
 
 cloudinary.v2.config({
