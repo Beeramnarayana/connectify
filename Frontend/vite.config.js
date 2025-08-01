17c27e495ba7b3d191c9ed2d5dbdcbcd1b9b65f9
narayana <beearamnarayan2002@mail.com>
2025-08-01 17:05:47 +0530

    Fix Vercel deployment configuration - Update routing and build settings

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:5000',
-        }
-      },
-      // host: '0.0.0.0',
-      // allowedHosts: ['social-media-7-t580.onrender.com','https://social-media-3-oj14.onrender.com'],
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      }
     },
-  //   esbuild: {
-  //     jsxInject: `import React from 'react'`,
-  //  },
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
