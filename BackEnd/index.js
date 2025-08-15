082ec089b2492812a373b770ac5fa672003ee1cf
Beeram Narayana <146806185+Beeramnarayana@users.noreply.github.com>
2025-08-15 20:13:41 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 // CORS configuration for production
 app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://connectify-mu-eight.vercel.app', 'https://connectify-vercel.vercel.app', 'https://connectify.vercel.app', 'https://connectify-git-main-beeramnarayana.vercel.app'] // Updated with actual domain
+    ? ['https://social-media-frontend-theta-sandy.vercel.app' ,'https://connectify-mu-eight.vercel.app', 'https://connectify-vercel.vercel.app', 'https://connectify.vercel.app', 'https://connectify-git-main-beeramnarayana.vercel.app'] // Updated with actual domain
     : ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
