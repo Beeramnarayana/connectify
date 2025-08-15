ef167d19ccb62b23197c41f6d517d9aae74a48e7
Beeram Narayana <146806185+Beeramnarayana@users.noreply.github.com>
2025-08-15 20:11:45 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     cors: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://connectify-8.onrender.com',
         changeOrigin: true,
         secure: false,
       }
